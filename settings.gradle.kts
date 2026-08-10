402fab874f324ad75794782eb43b2602a66bc296
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-10 21:14:43 +0300

    fix build dependency resolution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,8 +28,8 @@ gradle.rootProject {
                 force("io.netty:netty-codec-http2:4.2.17.Final")
                 force("io.netty:netty-codec:4.2.17.Final")
                 force("io.netty:netty-handler-proxy:4.2.17.Final")
-                force("org.bouncycastle:bcpkix-jdk18on:1.85.2")
-                force("org.bouncycastle:bcprov-jdk18on:1.85.2")
+                force("org.bouncycastle:bcpkix-jdk18on:1.85")
+                force("org.bouncycastle:bcprov-jdk18on:1.85")
                 force("ch.qos.logback:logback-core:1.6.1")
                 force("ch.qos.logback:logback-classic:1.6.1")
             }
@@ -40,8 +40,8 @@ gradle.rootProject {
                 force("io.netty:netty-codec-http2:4.2.17.Final")
                 force("io.netty:netty-codec:4.2.17.Final")
                 force("io.netty:netty-handler-proxy:4.2.17.Final")
-                force("org.bouncycastle:bcpkix-jdk18on:1.85.2")
-                force("org.bouncycastle:bcprov-jdk18on:1.85.2")
+                force("org.bouncycastle:bcpkix-jdk18on:1.85")
+                force("org.bouncycastle:bcprov-jdk18on:1.85")
                 force("ch.qos.logback:logback-core:1.6.1")
                 force("ch.qos.logback:logback-classic:1.6.1")
             }
