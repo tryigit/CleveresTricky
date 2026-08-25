48ed9ce52053372d7998415d38469be4681f359c
Yiğit <40565628+tryigit@users.noreply.github.com>
2026-08-26 01:40:11 +0300

    test: validate custom-template localization in split UX core

--- module/webui-tests/custom-template-localization.test.js
+++ module/webui-tests/custom-template-localization.test.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const assert = require('assert');
-const ux = fs.readFileSync('module/template/webroot/ux.js','utf8');
+const uxLoader = fs.readFileSync('module/template/webroot/ux.js', 'utf8');
+const uxCore = fs.readFileSync('module/template/webroot/ux-core.js', 'utf8');
+const ux = `${uxLoader}\n${uxCore}`;
 const required = [
   'Custom Templates','Create a reusable device identity template. The form stays collapsed until you open it.',
   'Template ID','Manufacturer','Fingerprint','Brand','Product','Device','Android release','Build ID','Incremental',
